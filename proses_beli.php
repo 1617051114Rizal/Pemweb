2492bbd1e673cd76fda724c537b0a77e5759b306
1617051065randhi yuansyah <37867019+1617051065Randhi@users.noreply.github.com>
2018-05-14 09:59:14 +0700

    Update proses_beli.php

--- proses_beli.php
+++ proses_beli.php
@@ -1,41 +1,17 @@
 <?php
-  //memanggil db_con.php untuk melakukan koneksi ke database
-	include "db_con.php";
+    include "koneksi.php";
+    $harga = $_POST['Bayar'];
+    $nama = $_POST['Nama'];
+    $email = $_POST['Email'];
+    $alamat = $_POST['Alamat'];
+    $provinsi = $_POST['Provinsi'];
+    $kabupaten = $_POST['Kabkot'];
+    $pembayaran = $_POST['Bank'];
+    $rekening = $_POST['Rekening'];
+    $hp = $_POST['Hp'];
+    $kategori = $_POST['Kategori'];
+    $nambar = $_POST['Nambar'];
+    $input = mysqli_query($koneksi, "INSERT INTO pembeli (nama, email, alamat, provinsi, kabupaten_kota, no_hp, metode_pembayaran, rekening, bayar, kategori, nama_barang) 
+    VALUES('$nama', '$email', '$alamat', '$provinsi', '$kabupaten', '$hp', '$pembayaran', '$rekening', '$harga', '$kategori', '$nambar')");
+    header("location:index_customer.php");
 ?>
-<html>
-	<head>
-		<title>Laporan Penjualan</title>
-	</head>
-	<body>
-		<h1>Laporan Penjualan</h1>
-		<table border="1">
-			<tr>
-				<td width="100px">ID Barang</td>
-				<td width="150px">Nama Barang</td>
-				<td width="100px">View</td>
-			</tr>
-			<?php
-				//melakukan query untuk mendapatkan data dari database penjualan
-				$query = "select * from penjualan";
-				$hasil = mysql_query($query);
-				
-				if ($hasil === FALSE) {
-					die(mysql_error());
-				}
-				while ($baris=mysql_fetch_array($hasil)) {
-			?>
-			<tr>
-				<!--
-				Menampilkan Hasil query dalam tabel
-				-->
-				<td><?php echo $baris['id_barang'] ?></td>
-				<td><?php echo $baris['nama_barang'] ?></td>
-				<!--
-				Mengirimkan infomasi berupa id barang untuk menampilkan chart secara spesifik menggunakan metode get
-				-->
-				<td><a href="chart.php?id_barang=<?php echo $baris['id_barang'] ?>">Chart !</a></td>
-				<?php } ?>
-			</tr>
-		</table>
-	</body>
-</html>
